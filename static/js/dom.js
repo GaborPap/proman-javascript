f36603f35320415a2566bdcc3cba80a71a956cb8
Roy91 <rolonew@gmail.com>
2019-05-16 09:49:12 +0200

    tolni

--- static/js/dom.js
+++ static/js/dom.js
@@ -26,30 +26,49 @@ export let dom = {
             dom.showBoards(boards);
         });
     },
+
     showBoards: function (boards) {
         // shows boards appending them to #boards div
         // it adds necessary event listeners also
 
-        const boardContainer = document.querySelector('#boards');
+        const boardContainer = document.querySelector('.board-container');
         boardContainer.innerHTML = '';
 
         for (let board of boards) {
+
             let template = document.querySelector('#board_header');
             let clone = document.importNode(template.content, true);
-            let section = document.createElement('section');
-            section.classList.add('board');
+            let section = document.createElement(`section`);
+            section.id = `board${board.id}`;
+            section.classList.add(`board`);
             clone.querySelector('.board-title').innerHTML = board.title;
             section.appendChild(clone);
             boardContainer.appendChild(section);
+            dom.loadCards(board.id);
         }
-        //this._appendToElement(document.querySelector('#boards'), outerHtml);
     },
+
     loadCards: function (boardId) {
-        // retrieves cards and makes showCards called
+        dataHandler.getCardsByBoardId(boardId, function () {
+            dom.showCards()
+        })
+
+            // dom.showCards(board_id);
+            // })
     },
     showCards: function (cards) {
         // shows the cards of a board
         // it adds necessary event listeners also
+        console.log(cards)
+        const section = document.querySelector(`board${id}`);
+
+        for (let card of cards){
+            let template_column= document.querySelector('#board_columns');
+            let clone_template = document.importNode(template_column.content, true);
+            clone_template.querySelector('.board-column-title').innerHTML = card.title;
+            section.appendChild(clone_template)
+        }
+
     },
     // here comes more features
 };
